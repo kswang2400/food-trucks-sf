1bbe0ead83f3d8432631a0c95d9ad5744e21ab3b
Kevin <kevinwang2400@gmail.com>
2015-08-09 12:17:34 -0700

    scraping data good

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,8 +1,21 @@
 
 class DataController < ApplicationController
   def index
-    client = SODA::Client.new({:domain => "https://data.sfgov.org/resource/rqzj-sfat.json", :app_token => ENV["socrata_app_token"]})
+    # was { domain => https://data.sfgov.org }
+    # https://www.ruby-forum.com/topic/152151
+    client = SODA::Client.new({:domain => "data.sfgov.org", :app_token => ENV["socrata_app_token"]})
+    response = client.get("6a9r-agq8")
+
+    render json: response
+
+    # i should learn how to format my own request
     
-    render json: client
+    # require 'net/http'
+    # url = URI.parse('http://www.example.com/index.html')
+    # req = Net::HTTP::Get.new(url.to_s)
+    # res = Net::HTTP.start(url.host, url.port) {|http|
+    #   http.request(req)
+    # }
+    # puts res.body
   end
 end
