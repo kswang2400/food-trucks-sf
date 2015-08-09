ca8b9495def1fb3b4c8073f3b3a7d995913f172d
Kevin <kevinwang2400@gmail.com>
2015-08-09 11:57:17 -0700

    set up soda client

--- app/controllers/data_controller.rb
+++ app/controllers/data_controller.rb
@@ -1,14 +1,8 @@
-require 'net/http'
 
 class DataController < ApplicationController
   def index
-    url = URI.parse("https://data.sfgov.org/resource/rqzj-sfat.json")
-    req = Net::HTTP::Get.new(url.to_s)
-    res = Net::HTTP.start(url.host, url.port) {|http|
-      http.request(req)
-    }
-    puts res.body
-
-    render json: res.body
+    client = SODA::Client.new({:domain => "https://data.sfgov.org/resource/rqzj-sfat.json", :app_token => ENV["socrata_app_token"]})
+    
+    render json: client
   end
 end
