2d0bd8f8424bb4e782cd1085a08f31ffb5786457
Kevin <kevinwang2400@gmail.com>
2015-08-10 13:53:16 -0700

    querying in SoQL working

--- app/controllers/api/data_controller.rb
+++ app/controllers/api/data_controller.rb
@@ -2,14 +2,25 @@ module Api
   class DataController < ApplicationController
     def index
       client = SODA::Client.new({:domain => "data.sfgov.org", :app_token => ENV["socrata_app_token"]})
-      response = client.get("6a9r-agq8", 
-        { "$limit" => 100, 
-          "$where" => "latitude IS NOT NULL AND longitude IS NOT NULL" })
-      
+
       if params[:location]
         # DO SOMETHING WITH LOCATION location[:longitude], location[:latitude]
+        lat = params[:location][:latitude].to_f
+        long = params[:location][:longitude].to_f
+        query_string = "latitude > #{lat - 0.1} AND latitude < #{lat + 0.1} AND longitude > #{long - 0.1} AND longitude < #{long + 0.1}"
+        
+        response = client.get("6a9r-agq8",
+          { "$limit" => 20,
+            "$where" => query_string})
+
+        response = "No trucks near there, where you at?" if response.empty?
+      elsif
+        response = client.get("6a9r-agq8", 
+          { "$limit" => 20, 
+            "$where" => "latitude IS NOT NULL AND longitude IS NOT NULL" })
       end
-      fail
+      # fail
+      # redirect_to "/#index"
       render json: response
     end
   end
