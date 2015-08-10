19bb8421ac89b43342990449c217a985a8af4037
Kevin <kevinwang2400@gmail.com>
2015-08-10 10:59:29 -0700

    manually precompile again

---
+++ public/assets/index-b89d58d56971c68c699f7c9fe8a95dfebff6dde4c86449a62391c4da073db3ff.js
@@ -0,0 +1,2 @@
+(function() { this.JST || (this.JST = {}); this.JST["index"] = function(obj){var __p=[],print=function(){__p.push.apply(__p,arguments);};with(obj||{}){__p.push('<h1>All ',  trucks.length ,' Trucks</h1>\n');  trucks.each(function (truck) { ; __p.push('\n  ',  truck.escape("applicant") ,'<br>\n');  }) ; __p.push('\n');}return __p.join('');};
+}).call(this);
