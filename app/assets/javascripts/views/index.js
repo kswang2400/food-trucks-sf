8bd90a1515415f6d1a28c4a93f712639f4b7e9a7
Kevin <kevinwang2400@gmail.com>
2015-08-11 12:21:41 -0700

    updating map works!

--- app/assets/javascripts/views/index.js
+++ app/assets/javascripts/views/index.js
@@ -9,16 +9,21 @@ FoodTrucks.Views.Index = Backbone.CompositeView.extend({
   initialize: function (options) {
     this.collection = options.trucks;
 
-    this.listenTo(this.collection, "sync", this.render);
-    this.listenTo(this.collection, "add", this.addTruckListItemSubview);
-
-    var map;
+    // this.listenTo(this.collection, "sync", this.render);
+    // this.listenTo(this.collection, "add", this.addTruckListItemSubview);
 
     google.maps.event.addDomListener(window, 'load', function () {
-      map = new google.maps.Map(document.getElementById('map-canvas'), {
+      this.map = new google.maps.Map(document.getElementById('map-canvas'), {
         zoom: 14,
         center: {lat: 37.78, lng: -122.41}
-      }); 
+      });
+
+      var myLatlng = new google.maps.LatLng(37.78 ,-122.41);
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: map,
+        title: "You are here!"
+      });
     });
   },
 
@@ -27,8 +32,37 @@ FoodTrucks.Views.Index = Backbone.CompositeView.extend({
     this.addSubview("#trucks-list", subview);
   },
 
+  addTruckMarker: function (trucks) {
+    var that = this;
+
+    trucks.each(function (truck) {
+      var location = truck.get("location")
+      var longitude = location["coordinates"][0]
+      var latitude = location["coordinates"][1]
+      var name = truck.get("applicant")
+      console.log(name, longitude, latitude);
+
+      var myLatlng = new google.maps.LatLng(latitude, longitude);
+      var marker = new google.maps.Marker({
+        position: myLatlng,
+        map: that.map,
+        title: name
+      });
+    });
+
+    $("#spinner").hide();
+  },
+
+  handleError: function () {
+    $("#error-message").show();
+    $("#spinner").hide();
+  },
+
   searchByCurrentLocation: function () {
+    var that = this;
+
     $("#spinner").show();
+
     navigator.geolocation.getCurrentPosition(function (pos) { 
       var latitude = pos.coords.latitude
       var longitude = pos.coords.longitude
@@ -37,15 +71,12 @@ FoodTrucks.Views.Index = Backbone.CompositeView.extend({
         longitude: longitude
       }}
 
-      this.collection.fetch({ 
-        data: query, 
-        error: function (col, err) {
-          console.log(err.responseText);
-          $("#error-message").show();
-          $("#spinner").hide();
-        }
+      that.collection.fetch({ 
+        data: query,
+        success: that.addTruckMarker, 
+        error: that.handleError
       });     
-    }.bind(this));
+    });
   },
 
   render: function () {
