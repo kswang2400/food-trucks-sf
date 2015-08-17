ed26d156d003656e848e4ea4b8feb6483eae5d7c
Kevin <kevinwang2400@gmail.com>
2015-08-17 09:51:12 -0700

    fix refresh

--- app/assets/javascripts/views/index.js
+++ app/assets/javascripts/views/index.js
@@ -9,10 +9,6 @@ FoodTrucks.Views.Index = Backbone.CompositeView.extend({
     "click #search-input": "searchInput"
   },
 
-  initialize: function () {
-    // this.listenTo(this.collection, "add", this.addTruckListItem);
-  },
-
   addTruckListItem: function (truck) {
     var subview = new FoodTrucks.Views.TruckListItem({ model: truck });
     this.addSubview("#truck-list", subview);
@@ -22,52 +18,22 @@ FoodTrucks.Views.Index = Backbone.CompositeView.extend({
     var that = this;
 
     trucks.each(function (truck) {
-      var location = truck.get("location")
-      var longitude = location["coordinates"][0]
-      var latitude = location["coordinates"][1]
-      var name = truck.get("applicant")
-      // console.log(name, longitude, latitude, truck.get("status"));
-
-      // Available Data
-      // address: "865 MARKET ST"
-      // applicant: "Kettle Corn Star"
-      // approved: "2015-04-23T13:59:20.000"
-      // block: "3705"
-      // blocklot: "3705 042"
-      // cnn: "8747103"
-      // expirationdate: "2016-03-15T00:00:00.000"
-      // facilitytype: "Push Cart"
-      // fooditems: "Kettle Corn: Funnel Cakes: Waffles: Lemonade"
-      // latitude: "37.7839469079742"
-      // location: Object
-      // locationdescription: "MARKET ST: POWELL ST to 05TH ST \ CYRIL MAGNIN ST (865 - 899) -- SOUTH --"
-      // longitude: "-122.407158344984"
-      // lot: "042"
-      // objectid: "648393"
-      // permit: "15MFF-0123"
-      // priorpermit: "0"
-      // received: "Apr 23 2015 1:46PM"
-      // schedule: "http://bsm.sfdpw.org/PermitsTracker/reports/report.aspx?title=schedule&report=rptSchedule&params=permit=15MFF-0123&ExportPDF=1&Filename=15MFF-0123_schedule.pdf"
-      // status: "APPROVED"
-      // x: "6010561.591"
-      // y: "2113530.347"
+      var location = truck.get("location");
+      var longitude = location["coordinates"][0];
+      var latitude = location["coordinates"][1];
 
       var contentString = that.infoWindowTemplate({ truck: truck });
-
-      var infowindow = new google.maps.InfoWindow({
-        content: contentString
-      });
-
+      var infowindow = new google.maps.InfoWindow({ content: contentString });
+      
       var myLatlng = new google.maps.LatLng(latitude, longitude);
+      var name = truck.get("applicant");
       var marker = new google.maps.Marker({
         position: myLatlng,
         map: that.map,
         title: name
       });
 
-      marker.addListener('click', function() {
-        infowindow.open(that.map, marker);
-      });
+      marker.addListener("click", function() { infowindow.open(that.map, marker) });
 
       that.addTruckListItem(truck);
     });
@@ -82,7 +48,7 @@ FoodTrucks.Views.Index = Backbone.CompositeView.extend({
 
   initializeMap: function () {
     var that = this;
-    this.map = new google.maps.Map(document.getElementById('map-canvas'), {
+    this.map = new google.maps.Map(document.getElementById("map-canvas"), {
       zoom: 14,
       center: {lat: this.latitude, lng: this.longitude}
     });
@@ -91,23 +57,16 @@ FoodTrucks.Views.Index = Backbone.CompositeView.extend({
     var searchBox = new google.maps.places.SearchBox(input);
     this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
     
-    this.map.addListener('bounds_changed', function() {
-      searchBox.setBounds(this.map.getBounds());
-    }.bind(this));
+    this.map.addListener("bounds_changed", function() { searchBox.setBounds(this.map.getBounds()) }.bind(this));
 
     var markers = [];
 
-    searchBox.addListener('places_changed', function() {
+    searchBox.addListener("places_changed", function() {
       var places = searchBox.getPlaces();
-
-      if (places.length == 0) {
-        return;
-      }
+      if (places.length == 0) { return }
 
       // Clear out the old markers.
-      markers.forEach(function(marker) {
-        marker.setMap(null);
-      });
+      markers.forEach(function(marker) { marker.setMap(null) });
       markers = [];
 
       // For each place, get the icon, name and location.
@@ -189,7 +148,7 @@ FoodTrucks.Views.Index = Backbone.CompositeView.extend({
   },
 
   refreshPage: function () {
-    window.location.reload()
+    window.location.href = "";
   },
 
   render: function () {
